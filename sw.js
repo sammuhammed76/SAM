0d62f4badd0b2f19417860cf56d9616a4a073a30
sammuhammed76 <sam_muhammed@hotmail.com>
2025-10-28 19:33:23 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,18 @@
-// SW v3 - cache'i kır
-const CACHE = "sam-v3";
+// SW v4 - cache break
+const CACHE = "sam-v4";
 const ASSETS = ["./","./index.html","./manifest.json"];
 
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting())
   );
 });
-
 self.addEventListener("activate", e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
   );
 });
-
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
