1c3409f0efee56af768f7007d6da8599bad16709
sammuhammed76 <sam_muhammed@hotmail.com>
2025-10-28 18:09:43 +0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,19 @@
+// Basit önbellek: ilk ziyaret sonrası offline açılış hızlanır
+const CACHE = "sam-v1";
+const ASSETS = ["./","./index.html","./manifest.json"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
