93cd2abdf6bf6f126a6fe837e5c6582759449bd3
sammuhammed76 <sam_muhammed@hotmail.com>
2025-10-28 19:26:00 +0300

    Upgrade service worker cache version and logic
    Updated service worker to use a new cache version and improved activation logic.

--- sw.js
+++ sw.js
@@ -1,14 +1,18 @@
-// Basit önbellek: ilk ziyaret sonrası offline açılış hızlanır
-const CACHE = "sam-v1";
+// SW v3 - cache'i kır
+const CACHE = "sam-v3";
 const ASSETS = ["./","./index.html","./manifest.json"];
 
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener("activate", e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
 
